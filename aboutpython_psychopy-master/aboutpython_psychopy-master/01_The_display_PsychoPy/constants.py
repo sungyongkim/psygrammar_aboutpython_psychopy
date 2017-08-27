169bce4cb675a916fca308f8cd653561a60b7653
sungyongkim <pingonfefalas@gmail.com>
2017-08-27 21:01:41 +0900

    delete and commit again

--- aboutpython_psychopy-master/aboutpython_psychopy-master/01_The_display_PsychoPy/constants.py
+++
@@ -1,6 +0,0 @@
-# Window resolution.
-DISPSIZE = (1920, 1080)
-
-# Default foreground and background colour.
-FGC = (0, 0, 0)
-BGC = (128, 128, 128)
--- aboutpython_psychopy-master/aboutpython_psychopy-master/README.md
+++
@@ -1,2 +0,0 @@
-# aboutpython_psychopy
-어바웃 파이썬 : PsychoPy
